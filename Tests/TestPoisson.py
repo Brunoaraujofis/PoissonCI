bb7b835b351929cb62395f6b7e54b44b82dea062
Pietro Chimenti <pietro.chimenti@gmail.com>
2021-12-14 16:49:27 -0300

    Plot test coverage

--- Tests/TestPoisson.py
+++ Tests/TestPoisson.py
@@ -6,26 +6,41 @@ Created on Tue Nov 23 13:38:09 2021
 @author: P.Chimenti, B.Araujo, R.Bassi
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 from Models import Poisson
-import scipy.stats as st
 
-def Coverage_Upper(mu, confidence):
-    number_of_samples = 100
-    success = 0
-    for sample in range(number_of_samples):
-        simulated_value = float(st.poisson.rvs(mu, loc=0, size=1))
-        upper_limit = Poisson.UpperLimit(simulated_value, ConfidenceLevel)
-        if upper_limit >= mu:
-            success += 1
-    return success/number_of_samples
-        
-        
-true_value = 10
-ConfidenceLevel = 0.95
 
-print("Confidence covarage: mu = ", true_value, " confidence = ", ConfidenceLevel, " coverage = ", Coverage_Upper(true_value, ConfidenceLevel))
+def Coverage_Upper(mu):
+    print("Coverage ",mu)
+    number_of_samples = 10
+    number_of_iterations = 8
+    success = np.zeros(number_of_iterations)
+    
+    for iteration in range(number_of_iterations):
+        for sample in range(number_of_samples):
+            simulated_value = float(stats.poisson.rvs(mu, loc=0, size=1))
+            upper_limit = Poisson.UpperLimit(simulated_value, ConfidenceLevel = 0.95)
+            if upper_limit >= mu:
+                success[iteration] += 1
+    mean_success = np.mean(success/number_of_samples)
+    var_success = np.var(success/number_of_samples)
+    return mean_success, np.sqrt(var_success)
 
-true_value = 1
-ConfidenceLevel = 0.95
+param_points = [0.1]
+L = list(np.arange(1.0, 11.0, 1))
 
-print("Confidence covarage: mu = ", true_value, " confidence = ", ConfidenceLevel, " coverage = ", Coverage_Upper(true_value, ConfidenceLevel))
+for i in np.arange(10):
+    param_points.append(L[i])
+
+Coverage = []
+Coverage_error = []
+
+for i in np.arange(len(param_points)):
+    result_coverage = Coverage_Upper(param_points[i]) 
+    Coverage.append(result_coverage[0])
+    Coverage_error.append(result_coverage[1])
+    
+plt.errorbar(param_points, Coverage, Coverage_error)
+plt.show()    
